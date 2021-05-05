d6cdfa9b1b974115080823cd9fa5b8e58fd0e2fb
ansi123 <mohitsinghalcool1998@gmail.com>
2021-05-05 13:57:35 +0530

    initial commit

---
+++ src/app/post.actions.ts
@@ -0,0 +1,11 @@
+import {Action} from '@ngrx/store';
+
+export const EDIT_TEXT = '[Post] Edit';
+export const UPVOTE = '[Post] Upvote';
+export const DOWNVOTE = '[Post] Downvote';
+export const RESET = '[Post] Reset';
+
+
+export class EditText implements Action{
+    readonly 
+}
